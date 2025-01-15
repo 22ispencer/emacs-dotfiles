f5b18a641132aed592c96f5bdef914d478591fb7
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-01-15 17:57:21 -0600

    emacs config

---
+++ init.el
@@ -0,0 +1,348 @@
+;; The default is 800 kilobytes. Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+(defun start/org-babel-tangle-config ()
+  "Automatically tangle our Emacs.org config file when we save it. Credit to Emacs From Scratch for this one!"
+  (when (string-equal (file-name-directory (buffer-file-name))
+                      (expand-file-name user-emacs-directory))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (add-hook 'after-save-hook
+                      #'start/org-babel-tangle-config)))
+
+; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+; Setup use-package
+(setq straight-use-package-by-default t)
+(straight-use-package 'use-package)
+
+(use-package evil
+  :init
+  ;; Execute code Before a package is loaded
+  (evil-mode)
+  :config ;; Execute code After a package is loaded
+  (evil-set-initial-state 'eat-mode 'insert) ;; Set initial state in eat terminal to insert mode
+  :custom ;; Customization of package custom variables
+  (evil-want-keybinding nil)    ;; Disable evil bindings in other modes (It's not consistent and not good)
+  (evil-want-C-u-scroll t)      ;; Set C-u to scroll up
+  (evil-want-C-i-jump nil)      ;; Disables C-i jump
+  (evil-undo-system 'undo-redo) ;; C-r to redo
+  (org-return-follows-link t)   ;; Sets RETURN key in org-mode to follow links
+  ;; Unmap keys in 'evil-maps. If not done, org-return-follows-link will not work
+  :bind (:map evil-motion-state-map
+              ("SPC" . nil)
+              ("RET" . nil)
+              ("TAB" . nil)))
+(use-package evil-collection
+  :after
+  evil
+  :config
+  ;; Setting where to use evil-collection
+  (setq evil-collection-mode-list
+        '(dired ibuffer magit corfu vertico consult))
+  (evil-collection-init))
+
+(use-package evil-surround
+  :config
+  (global-evil-surround-mode 1))
+
+(use-package evil-commentary
+  :config
+  (evil-commentary-mode))
+
+(use-package evil-snipe
+  :config 
+  (evil-snipe-mode 1)
+  (evil-snipe-override-mode 1)
+  (setq evil-snipe-spillover-scope 'visible))
+
+(use-package general
+  :config
+  (general-evil-setup)
+  ;; Set up 'SPC' as the leader key
+  (general-create-definer start/leader-keys
+    :states
+    '(normal insert visual motion emacs)
+    :keymaps 'override
+    :prefix "SPC"           ;; Set leader key
+    :global-prefix "C-SPC")
+  ;; Set global leader key
+
+  (start/leader-keys
+    "." '(find-file :wk "Find file")
+    "TAB" '(comment-line :wk "Comment lines")
+    "p" '(projectile-command-map :wk "Projectile command map"))
+
+  (start/leader-keys
+    "f" '(:ignore t :wk "Find")
+    "f c"
+    '((lambda () (interactive)
+        (find-file "~/.config/emacs/config.org"))
+      :wk "Edit emacs config")
+    "f r" '(consult-recent-file :wk "Recent files")
+    "f f" '(consult-fd :wk "Fd search for files")
+    "f g" '(consult-ripgrep :wk "Ripgrep search in files")
+    "f l" '(consult-line :wk "Find line")
+    "f i" '(consult-imenu :wk "Imenu buffer locations"))
+
+  (start/leader-keys
+    "b" '(:ignore t :wk "Buffer Bookmarks")
+    "b b" '(consult-buffer :wk "Switch buffer")
+    "b k" '(kill-this-buffer :wk "Kill this buffer")
+    "b i" '(ibuffer :wk "Ibuffer")
+    "b n" '(next-buffer :wk "Next buffer")
+    "b p" '(previous-buffer :wk "Previous buffer")
+    "b r" '(revert-buffer :wk "Reload buffer")
+    "b j" '(consult-bookmark :wk "Bookmark jump"))
+  (start/leader-keys
+	"c" '(:ignore t :wh "Code")
+	"c f" '(format-all-buffer :wk "Format"))
+
+  (start/leader-keys
+    "d" '(:ignore t :wk "Dired")
+    "d v" '(dired :wk "Open dired")
+    "d j" '(dired-jump :wk "Dired jump to current"))
+
+  (start/leader-keys
+    "e" '(:ignore t :wk "Eglot Evaluate")
+    "e e" '(eglot-reconnect :wk "Eglot Reconnect")
+    "e f" '(eglot-format :wk "Eglot Format")
+    "e l" '(consult-flymake :wk "Consult Flymake")
+    "e b" '(eval-buffer :wk "Evaluate elisp in buffer")
+    "e r" '(eval-region :wk "Evaluate elisp in region"))
+
+  (start/leader-keys
+    "g" '(:ignore t :wk "Git")
+    "g g" '(magit-status :wk "Magit status"))
+
+  (start/leader-keys
+    "h" '(:ignore t :wk "Help")
+    ;; To get more help use C-h commands (describe variable, function, etc.)
+    "h q" '(save-buffers-kill-emacs :wk "Quit Emacs and Daemon")
+    "h r" '((lambda () (interactive)
+              (load-file "~/.config/emacs/init.el"))
+            :wk "Reload Emacs config"))
+  
+  (start/leader-keys
+	"n" '(:ignore t :wk "Roam")
+	"n l" '(org-roam-buffer-toggle :wk "Roam toggle buffer")
+	"n f" '(org-roam-node-find :wk "Roam find node")
+	"n i" '(org-roam-node-insert :wk "Roam create node"))
+
+  (start/leader-keys
+    "s" '(:ignore t :wk "Show")
+    "s e" '(eat :wk "Eat terminal"))
+
+  (start/leader-keys
+    "t" '(:ignore t :wk "Toggle")
+    "t t" '(visual-line-mode :wk "Toggle truncated lines (wrap)")
+    "t l" '(display-line-numbers-mode :wk "Toggle line numbers")))
+
+(use-package which-key
+  :init
+  (which-key-mode 1)
+  :diminish
+  :custom
+  (which-key-side-window-location 'bottom)
+  (which-key-sort-order #'which-key-key-order-alpha) ;; Same as default, except single characters are sorted alphabetically
+  (which-key-sort-uppercase-first nil)
+  (which-key-add-column-padding 1) ;; Number of spaces to add to the left of each column
+  (which-key-min-display-lines 6)  ;; Increase the minimum lines to display, because the default is only 1
+  (which-key-idle-delay 0.8)       ;; Set the time delay (in seconds) for the which-key popup to appear
+  (which-key-max-description-length 25)
+  (which-key-allow-imprecise-window-fit nil)) ;; Fixes which-key window slipping out in Emacs Daemon
+
+(use-package emacs
+  :custom
+  (menu-bar-mode nil)         ;; Disable the menu bar
+  (scroll-bar-mode nil)       ;; Disable the scroll bar
+  (tool-bar-mode nil)         ;; Disable the tool bar
+  ;;(inhibit-startup-screen t)  ;; Disable welcome screen
+
+  (delete-selection-mode t)   ;; Select text and delete it by typing.
+  ;; (electric-indent-mode nil)  ;; Turn off the weird indenting that Emacs does by default.
+  (electric-pair-mode t)      ;; Turns on automatic parens pairing
+
+  (blink-cursor-mode nil)     ;; Don't blink cursor
+  (global-auto-revert-mode t) ;; Automatically reload file and show changes if the file has changed
+
+  ;;(dired-kill-when-opening-new-dired-buffer t) ;; Dired don't create new buffer
+  ;;(recentf-mode t) ;; Enable recent file mode
+
+  ;;(global-visual-line-mode t)           ;; Enable truncated lines
+  (display-line-numbers-type 'relative) ;; Relative line numbers
+  (global-display-line-numbers-mode t)  ;; Display line numbers
+
+  (mouse-wheel-progressive-speed nil) ;; Disable progressive speed when scrolling
+  (scroll-conservatively 10) ;; Smooth scrolling
+  ;;(scroll-margin 8)
+
+  (tab-width 4)
+
+  (make-backup-files nil) ;; Stop creating ~ backup files
+  (auto-save-default nil) ;; Stop creating # auto save files
+  :hook
+  (prog-mode . (lambda () (hs-minor-mode t))) ;; Enable folding hide/show globally
+  :config
+  ;; Move customization variables to a separate file and load it, avoid filling up init.el with unnecessary variables
+  (setq custom-file (locate-user-emacs-file "custom-vars.el"))
+  (load custom-file 'noerror 'nomessage)
+  :bind (
+         ([escape] . keyboard-escape-quit) ;; Makes Escape quit prompts (Minibuffer Escape)
+         )
+  ;; Fix general.el leader key not working instantly in messages buffer with evil mode
+  :ghook ('after-init-hook
+          (lambda (&rest _)
+            (when-let
+                ((messages-buffer (get-buffer "*Messages*")))
+              (with-current-buffer messages-buffer
+                (evil-normalize-keymaps))))
+          nil nil t)
+  )
+
+(use-package monokai-theme
+      :config
+      (load-theme 'monokai t))
+;; We need to add t to trust this package
+
+(set-face-attribute 'default nil
+                    :font "Monaspace Argon" ;; Set your favorite type of font or download JetBrains Mono
+                    :height 200
+                    :weight 'medium)
+;; This sets the default font on all graphical frames created after restarting Emacs.
+;; Does the same thing as 'set-face-attribute default' above, but emacsclient fonts
+;; are not right unless I also add this method of setting the default font.
+
+;;(add-to-list 'default-frame-alist '(font . "JetBrains Mono")) ;; Set your favorite font
+(setq-default line-spacing 0.12)
+
+(use-package projectile
+	:init
+	(projectile-mode)
+	:custom
+	(projectile-run-use-comint-mode t) ;; Interactive run dialog when running projects inside emacs (like giving input)
+	(projectile-switch-project-action #'projectile-dired) ;; Open dired when switching to a project
+	(projectile-project-search-path
+         '("~/projects/" "~/work/" ("~/github" . 1))))
+;; . 1 means only search the first subdirectory level for projects
+    ;; Use Bookmarks for smaller, not standard projects
+
+;;(use-package eglot
+;;  :ensure nil ;; Don't install eglot because it's now built-in
+;;  :hook ((c-mode c++-mode ;; Autostart lsp servers for a given mode
+;;                 lua-mode) ;; Lua-mode needs to be installed
+;;         . eglot-ensure)
+;;  :custom
+;;  ;; Good default
+;;  (eglot-events-buffer-size 0) ;; No event buffers (Lsp server logs)
+;;  (eglot-autoshutdown t);; Shutdown unused servers.
+;;  (eglot-report-progress nil) ;; Disable lsp server logs (Don't show lsp messages at the bottom, java)
+;;  ;; Manual lsp servers
+;;  :config
+;;  (add-to-list 'eglot-server-programs
+;;               `(lua-mode . ("PATH_TO_THE_LSP_FOLDER/bin/lua-language-server" "-lsp"))) ;; Adds our lua lsp server to eglot's server list
+;;  )
+
+(use-package yasnippet-snippets
+  :hook
+  (prog-mode . yas-minor-mode))
+
+(use-package format-all
+  :commands format-all-mode
+  :hook (prog-mode . format-all-mode)
+  :config
+  (setq-default format-all-formatters
+                '(("C"     (astyle "--mode=c"))
+                  ("Shell" (shfmt "-i" "4" "-ci")))))
+
+(use-package org
+  :ensure
+  nil
+  :custom
+  (org-src-tab-acts-natively t)
+  (org-todo-keywords '((sequence
+						"TODO(t)" ; doing later
+						"NEXT(n)" ; doing now or next
+						"|"
+						"DONE(d)" ; complete
+						)
+					   (sequence
+						"WAIT(w)" ; unable to complete
+						"IDEA(i)" ; uncommited to it
+						"|"
+						"STOP(s)" ; decided to not complete
+						)))
+  (org-todo-keyword-faces '(("TODO" . "red")
+							("NEXT" . "yellow")
+							("DONE" . "green")
+							("WAIT" . "orange")
+							("IDEA" . "purple")
+							("STOP" . "cyan")))
+
+  :hook
+  (org-mode . org-indent-mode) ;; Indent text
+  ;; The following prevents <> from auto-pairing when electric-pair-mode is on.
+  ;; Otherwise, org-tempo is broken when you try to <s TAB...
+  (org-mode . (lambda ()
+                (setq-local electric-pair-inhibit-predicate
+                            `(lambda (c)
+                               (if (char-equal c ?<) t
+                                 (,electric-pair-inhibit-predicate
+                                  c))))))
+
+  )
+
+(use-package toc-org
+    :commands
+    toc-org-enable
+    :hook (org-mode . toc-org-mode))
+
+(use-package org-superstar
+    :after
+    org
+    :hook (org-mode . org-superstar-mode))
+
+(use-package org-tempo
+  :ensure
+  nil
+  :straight nil
+  :after org)
+
+(use-package org-roam
+  :custom
+  (org-roam-directory "~/org-roam")
+  (org-roam-database-connector 'sqlite-builtin)
+:config
+(org-roam-setup))
+
+(use-package helm
+  :config
+  (helm-mode))
+
+(use-package magit
+  :commands magit-status)
+
+;; (use-package aggressive-indent)
+
+(use-package rainbow-mode
+  :config
+  (rainbow-mode))
